4ab827c18fb5e657b0d0d4c96b9adfc085156dd9
Nisha Bhuva <nishabhuva@Nishas-MBP.T-mobile.com>
2019-11-23 19:13:30 -0600

    added api

--- vue.js
+++ vue.js
@@ -2,7 +2,14 @@ window.onload = function () {
   const app = new Vue ({
     el: '#app',
     data: {
-      product: 'Boots'
+      products: []
+    },
+    created () {
+      fetch('https://api.myjson.com/bins/74l63')
+        .then(response => response.json())
+        .then(json => {
+          this.products = json.products
+        })
     }
   })
 }
